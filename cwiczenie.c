5494bd795a747cb85fcf0b72ae02b44664a134f8
Mikolaj <245882@edu.p.lodz.pl>
2022-10-20 18:41:57 +0200

    Zamiana dodawania na mnozenie

--- cwiczenie.c
+++ cwiczenie.c
@@ -2,6 +2,6 @@
 
 int main() {
     printf("Hello, World!\n");
-    printf("W pierwszym branchu; 5 + 9 = %d",5+9);
+    printf("W drugim branchu; 5 * 9 = %d",5*9); //zamiana dodawania na mnozenie
     return 0;
 }
